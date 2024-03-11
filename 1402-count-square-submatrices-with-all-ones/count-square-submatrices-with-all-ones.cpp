5530c7525e55406cfec53283e55703d08d245ac6
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-03-11 12:37:15 +0530

    Time: 48 ms (54.25%) | Memory: 26.1 MB (76.53%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
+            {
+                if(matrix[i][j] == 1)
+                {
+                    int mini = min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i][j-1]));
+                    matrix[i][j] = 1 + mini;
+                }
+            }
+        }
+        
+        int countSquares = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+            countSquares += matrix[i][j];
+        
+        return countSquares;
+    }
+};
