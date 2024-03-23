dd19cc4240fcf56a499a2ef1500ea32d656005b9
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-03-23 21:35:29 +0530

    Time: 28 ms (40.68%) | Memory: 23.6 MB (11.57%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,22 +1,21 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-#define ordered_set tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
-
 class KthLargest {
+private:
+    priority_queue <int, vector<int>, greater<int>> minHeap;
+    int index;
 public:
-    ordered_set st;
-    int index = 0;
     KthLargest(int k, vector<int>& nums) {
         index = k;
-        for(int ele: nums)    st.insert(ele);
+        for(int ele: nums)
+        {
+            minHeap.push(ele);
+        }
+        while(minHeap.size() > k)   minHeap.pop();
     }
     
     int add(int val) {
-        st.insert(val);
-        return *st.find_by_order(index-1);
+        minHeap.push(val);
+        if(minHeap.size() > index)   minHeap.pop();
+        return minHeap.top();
     }
 };
 
