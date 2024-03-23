291bd2f27f5f947798e55e302848ce4910895ff9
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-03-23 17:42:27 +0530

    Time: 12 ms (62.95%) | Memory: 17.2 MB (62.01%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,32 @@
+#define pi pair<int, int>
+
+class Solution {
+public:
+    struct cmp {
+        bool operator()(pair<int, int> a, pair<int, int> b) {
+            return a.second < b.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> mp;
+        for(int ele: nums)  mp[ele]++;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> pq;
+        for(auto it: mp)
+        {
+            // cout << it.first << " : " << it.second << " | ";
+            pq.push({it.first, it.second});
+        }    
+        cout << "\n";
+        vector<int> ans;
+        while(!pq.empty() && k--)
+        {
+            auto it = pq.top();
+            // cout << it.first << " -> " << it.second << " | ";
+            ans.push_back(it.first);
+            pq.pop();
+        }
+        return ans;
+    }
+};
