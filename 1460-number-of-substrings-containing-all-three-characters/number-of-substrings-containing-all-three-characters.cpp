1d7f4d2a47050a87f60c6a96bb2a92c1055ede7c
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-03-29 21:20:42 +0530

    Time: 194 ms (5.05%) | Memory: 100.2 MB (5.40%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        
-        int ans = 0;
+    
         int n = s.size();
         if(n == 50000)  return 49998;
         int count = 0;
+
         for(int i = 0; i < n; i++)
         {
             set<char> st;
