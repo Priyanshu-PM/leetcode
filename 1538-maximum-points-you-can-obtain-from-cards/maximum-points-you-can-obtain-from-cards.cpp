8dc671076b7871152e0f7416dbd7ab4d2f118b86
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-03-29 21:00:28 +0530

    Time: 36 ms (86.99%) | Memory: 45.6 MB (15.45%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
 
-        vector<int> temp = {96,90,41,82,39,74,64,50,30};
-        if(temp == cardPoints)  return 536;
+        // vector<int> temp = {96,90,41,82,39,74,64,50,30};
+        // if(temp == cardPoints)  return 536;
 
-        temp = {90,51,34,9,94,38,2,9,34,68,19,77,74,62,55,17};
-        if(temp == cardPoints)  return 90;
+        // temp = {90,51,34,9,94,38,2,9,34,68,19,77,74,62,55,17};
+        // if(temp == cardPoints)  return 90;
 
-        temp = {99,82,25,56,39,77,22,58,64,77,19,36,93,14,19,12,94,76,93,24,92,67,18,37,37,60,87,28,64,7,29};
-        if(temp == cardPoints)  return 1477;
+        // temp = {99,82,25,56,39,77,22,58,64,77,19,36,93,14,19,12,94,76,93,24,92,67,18,37,37,60,87,28,64,7,29};
+        // if(temp == cardPoints)  return 1477;
 
-        temp = {47,45,35,75,57,78,100,31,45,69,47,32,12,18,74,8,83,87,43,39,58,8,96,89,74,79,28,20,31,65,15,36,56,29,89,75,5,29,2,19,25,32};
-        if(temp == cardPoints)  return 1025;
-        
+        // temp = {47,45,35,75,57,78,100,31,45,69,47,32,12,18,74,8,83,87,43,39,58,8,96,89,74,79,28,20,31,65,15,36,56,29,89,75,5,29,2,19,25,32};
+        // if(temp == cardPoints)  return 1025;
 
         int n = cardPoints.size();
         vector<int> prefix(k+1, 0);
@@ -22,6 +21,7 @@ public:
 
         int maxScore = INT_MIN;
         int currSum = 0, index = n-1;
+        maxScore = max(prefix[k], currSum + prefix[0]);
         for(int i = k-1; i >= 0 && index >= 0; i--) {
             currSum += cardPoints[index];
             maxScore = max(maxScore, currSum + prefix[i]);
