5d0bbaebfd0224cffe3ded0c6fae597797005077
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-04-09 20:44:21 +0530

    Time: 2 ms (57.41%) | Memory: 9.3 MB (67.56%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,25 +2,36 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
-        queue<pair<int, int>> q;    //  for storing the {tickets left, index}
+        // queue<pair<int, int>> q;    //  for storing the {tickets left, index}
+        // for(int i = 0; i < tickets.size(); i++)
+        // {
+        //     q.push({tickets[i], i});
+        // }
+        // int time = 0;
+        // while(!q.empty())
+        // {
+        //     auto [tickets, index] = q.front();
+        //     q.pop();
+        //     time++;
+        //     if(tickets == 1)
+        //     {
+        //         if(index == k)  return time;
+        //     }
+        //     else
+        //     {
+        //         q.push({tickets - 1, index});
+        //     }
+        // }
+        // return time;
+
+        //  T.C = O(N)
+        int tick = tickets[k];
+        int time = 0;
         for(int i = 0; i < tickets.size(); i++)
         {
-            q.push({tickets[i], i});
-        }
-        int time = 0;
-        while(!q.empty())
-        {
-            auto [tickets, index] = q.front();
-            q.pop();
-            time++;
-            if(tickets == 1)
-            {
-                if(index == k)  return time;
-            }
-            else
-            {
-                q.push({tickets - 1, index});
-            }
+            if(i < k)   time += min(tickets[i], tick);
+            else if(i > k) time += min(tickets[i], tick-1);
+            else time += tickets[k];
         }
         return time;
     }
