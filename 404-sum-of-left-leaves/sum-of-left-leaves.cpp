61ea005770c8321d7ac656ce10beb7cd57fe010c
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-04-14 09:41:08 +0530

    Time: 3 ms (56.94%) | Memory: 14.5 MB (97.35%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -11,46 +11,18 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int &sum)
-    {
-        if(root == NULL)    return;
-        if(root->left == NULL && root->right == NULL)   sum += root->val;
-        
-        solve(root->left, sum);
-        if(root->right != NULL)
-        {
-            root = root->right;
-            solve(root->left, sum);
+    int dfs(TreeNode* node, bool isLeft) {
+        if (!node) {
+            return 0;
         }
+        if (!node->left && !node->right) { // Leaf node
+            return isLeft ? node->val : 0;
+        }
+        int leftSum = dfs(node->left, true);    // Traverse left child
+        int rightSum = dfs(node->right, false); // Traverse right child
+        return leftSum + rightSum;
     }
-
     int sumOfLeftLeaves(TreeNode* root) {
-        int sum = 0;
-        if(root->left == NULL && root->right == NULL)   return 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, -1});
-        while(!q.empty())
-        {
-            auto [node, leaf] = q.front();
-            q.pop();
-            if(leaf == 1)
-            {
-                if(node->left == NULL && node->right == NULL)
-                    sum += node->val;
-                else
-                {
-                    if(node->left)  q.push({node->left, 1});
-                    if(node->right) q.push({node->right, 0});
-                }
-            }
-            else
-            {
-                if(node->left)  q.push({node->left, 1});
-                if(node->right) q.push({node->right, 0});
-            }
-
-        }
-        // solve(root, sum);
-        return sum;
+        return dfs(root, false);
     }
 };
