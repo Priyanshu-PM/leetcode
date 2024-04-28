b97f92a44840daf189fa3d1772ac58ba4b850aa0
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-04-28 22:31:44 +0530

    Time: 216 ms (56.54%) | Memory: 103.6 MB (38.15%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int dfs(int node, vector<int> adj[], vector<int>& child, vector<int>& visited, int &res, int level)
+    {
+        visited[node] = 1;
+        res += level;
+
+        int children = 0;
+        for(auto neigh: adj[node])
+        {
+            if(!visited[neigh])
+            {
+                children += dfs(neigh, adj, child, visited, res, level + 1);
+            }
+        }
+        return child[node] = children + 1;
+    }
+
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        
+        vector<int> adj[n];
+        for(auto it: edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        int res = 0;
+        int level = 0;
+        vector<int> child(n, 0);
+        vector<int> visited(n, 0);
+
+        int temp = dfs(0, adj, child, visited, res, level);
+
+        vector<int> result(n, 0);
+        vector<int> visited1(n, 0);
+
+        queue<pair<int,int>> q;
+        q.push({0, 0});
+        visited1[0] = 1;
+
+        while(!q.empty())
+        {
+            auto front = q.front();
+            q.pop();
+            int node = front.first;
+            int parent = front.second;
+
+            if(node == 0) {
+                result[node] = res;
+            }
+            else
+            {
+                int parent_val = result[parent];
+                int upper_nodes = (n - child[node]);
+
+                result[node] = (parent_val - child[node] + upper_nodes);
+            }
+
+            for(auto neigh: adj[node])
+            {
+                if(!visited1[neigh]) {
+                    q.push({neigh, node});
+                    visited1[neigh] = 1;
+                }
+            }
+        }
+        return result;
+    }
+};
