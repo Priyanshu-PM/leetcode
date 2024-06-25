6abde1da265b8981319f740cf9752b96b3a5af4d
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-06-25 14:49:08 +0530

    Time: 2 ms (49.63%) | Memory: 10.2 MB (42.71%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -10,44 +10,14 @@
  * };
  */
 class Solution {
-private:
-    void inOrder(TreeNode* root, vector<int>& ls)
-    {
-        if(!root)   return;
-        inOrder(root->left, ls);
-        ls.push_back(root->val);
-        inOrder(root->right, ls);
-    }
-
 public:
+    int val = 0;
     TreeNode* bstToGst(TreeNode* root) {
         
-        if(!root)   return NULL;
-        vector<int> ls;
-        inOrder(root, ls);
-
-        unordered_map<int, int> mp;
-        int n = ls.size();
-        for(int i = n-1; i >= 0; i--) {
-            if(i < n-1) {
-                mp[ls[i]] = mp[ls[i+1]] + ls[i];
-            }
-            else {
-                mp[ls[i]] = ls[i];
-            }
-        }
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()) 
-        {
-            TreeNode* node = q.front();
-            q.pop();
-            node->val = mp[node->val];
-            if(node->left)  q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-        return root;
+        //  reverse inorder traversal
+        if(root->right) bstToGst(root->right);
+        val = root->val = val + root->val;
+        if(root->left)  bstToGst(root->left);
+        return root; 
     }
 };
