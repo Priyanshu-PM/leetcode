1cb4987395779067ed021b62a0ac57bf6eb19271
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-06-26 19:44:40 +0530

    Time: 9 ms (87.02%) | Memory: 15.8 MB (40.84%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,25 +1,30 @@
+//TC=O(nlogn)
 class Solution {
 public:
+int search(vector<int>& nums,int t,int s)
+{
+    int e=nums.size()-1;
+    while(s<=e)
+    {
+        int mid=s+(e-s)/2;
+        if(nums[mid]==t)
+            return mid;
+        else if(t>nums[mid])
+            s=mid+1;
+        else
+            e=mid-1;
+    }
+    return -1;
+}
     int findPairs(vector<int>& nums, int k) {
-        
-        int n = nums.size();
-        map<int, int> mp;
-        for(int ele: nums)  mp[ele]++;
-
-        int ans = 0;
-        for(auto it: mp)
+        sort(nums.begin(),nums.end());
+        set<pair<int,int>> ans;
+        int i=0;
+        for(int i=0;i<nums.size();i++)
         {
-            int prev = it.first - k;
-            int next = k + it.first;
-
-            if((prev != it.first && mp.find(prev) != mp.end()) || (prev == it.first && it.second > 1)) {
-                ans++;
-            }
-            if(next != it.first && mp.find(next) != mp.end()) {
-                ans++;
-            }
-            mp.erase(it.first);
+            if(search(nums,nums[i]+k,i+1)!=-1)
+            ans.insert({nums[i],nums[i]+k});
         }
-        return ans;
+        return ans.size();
     }
 };
