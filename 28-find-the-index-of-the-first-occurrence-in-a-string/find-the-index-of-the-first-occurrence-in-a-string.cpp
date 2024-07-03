4111263e415bf4ab97ea4134088920764642d019
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-03 18:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (29.35%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,50 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+
         int hlen = haystack.size();
-        int nlen = needle.size();
-        int nIndex = 0;
-        for(int i = 0; i < hlen; i++)
+        int nLen = needle.size();
+
+        //  KMP algorithm
+        vector<int> lps(nLen, 0);
+        lps[0] = 0;
+        int len = 0;
+        int i = 1;
+        while(i < nLen)
         {
-            if(haystack[i] == needle[nIndex])   nIndex++;
-            else
-            {
-                i = i - nIndex;
-                nIndex = 0;
+            if(needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
             }
-            if(nIndex == nlen)  return i - nlen + 1;
+            else {
+                if(len != 0) {
+                    len = lps[len-1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        for(int i = 0; i < nLen; i++)
+            cout << i << " -> " << lps[i] << "\n";
+
+        int nIndex = 0;
+        i = 0;
+        while(i < hlen)
+        {
+            if(haystack[i] == needle[nIndex]) {  nIndex++; i++;}
+            if(nIndex == nLen)  return i - nLen;
+            else if(haystack[i] != needle[nIndex])
+            {
+                if(nIndex != 0)
+                    nIndex = lps[nIndex - 1];
+                else {
+                    i++; 
+                }
+            }
         }
         return -1;
     }
