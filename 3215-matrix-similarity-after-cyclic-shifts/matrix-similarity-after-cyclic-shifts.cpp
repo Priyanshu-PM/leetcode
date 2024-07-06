f6a7081f4ab0adb0b3844834b004a537da62b8a0
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-06 11:34:35 +0530

    Time: 15 ms (84.30%) | Memory: 31.9 MB (29.33%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        
+        vector<vector<int>> temp = mat;
+        int n = temp.size();
+        int m = temp[0].size();
+        k = k % m;
+        for(int i = 0; i < n; i++)
+        {
+            if(i % 2 == 0) {
+                reverse(temp[i].end() - k, temp[i].end());
+                reverse(temp[i].begin(), temp[i].end() - k);
+                reverse(temp[i].begin(), temp[i].end());
+            }
+            else {
+                reverse(temp[i].begin(), temp[i].begin() + k);
+                reverse(temp[i].begin()+k, temp[i].end());
+                reverse(temp[i].begin(), temp[i].end());
+            }
+        }
+
+        return temp == mat;
+    }
+};
