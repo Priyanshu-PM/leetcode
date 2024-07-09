2eff2b8a1a97d2d0f23eb7bb7964037f9f83ad41
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-09 23:00:26 +0530

    Time: 30 ms (77.04%) | Memory: 38.4 MB (46.20%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int solve(vector<int>& arr, int index, vector<int>& vis)
+    {
+        if(index < 0 || index >= arr.size())    return 0;
+        vis[index] = 1;
+        if(arr[index] == 0) return 1;
+
+        int extra = arr[index];
+        if(index + extra < arr.size() && !vis[index + extra] && solve(arr, index + extra, vis))
+            return 1;
+        else if(index - extra >= 0 && !vis[index - extra] && solve(arr, index - extra, vis))
+            return 1;
+        return 0;
+    }
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<int> vis(n, 0);
+        return solve(arr, start, vis);
+    }
+};
