cfcd4f485868f35bb52100a86c0899e857f0b89d
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-10 22:20:58 +0530

    Time: 40 ms (48.45%) | Memory: 51 MB (13.30%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,29 +3,25 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size();
-        // cout << "n is : " << n << endl;
-        vector<vector<int>> dp(n + 1, vector<int>(amount+1, 0));
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
 
-        for(int i = 1; i <= amount; i++) {
+        //  0 cannot make up to any number
+        for(int i = 0; i <= amount; i++)
             dp[0][i] = 1e8;
-        }
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= amount; j++) {
-                
-                if(j < coins[i-1]) {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= amount; j++)
+            {
+                if(j < coins[i-1])
                     dp[i][j] = dp[i-1][j];
-                }
                 else {
                     dp[i][j] = min(dp[i-1][j], 1 + dp[i][j - coins[i-1]]);
                 }
             }
-        }   
+        }
 
-        cout << endl;
-        if(dp[n][amount] != 1e8)
-            return dp[n][amount];
-            
+        if(dp[n][amount] != 1e8)    return dp[n][amount];
         return -1;
     }
 };
