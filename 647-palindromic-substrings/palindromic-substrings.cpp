87081197392175c1782440a625a0a9e20fbea424
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-10 23:02:08 +0530

    Time: 38 ms (27.73%) | Memory: 10 MB (61.67%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,35 +1,60 @@
 class Solution {
 private:
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s) {
+    bool isPalindrome(string &s, int i, int j, vector<vector<int>>& dp)
+    {
+        if(i > j)   return 1;
 
-        if(i == j)
-            return dp[i][j] = true;
-        if(j - i == 1) {
-            if(s[i] == s[j])
-                return dp[i][j] = true;
-            else 
-                return dp[i][j] = false;
+        //  memoization
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        if(s[i] == s[j])
+        {
+            return dp[i][j] = isPalindrome(s, i+1, j-1, dp);
         }
-        if(s[i] == s[j] && dp[i+1][j-1] == true) {
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
+        return dp[i][j] = 0;
     }
-
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        int cnt = 0;
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // int cnt = 0;
+        // for(int i = 0; i < n; i++)
+        // {
+        //     for(int j = i; j < n; j++)
+        //     {
+        //         if(isPalindrome(s, i, j, dp))    cnt++;
+        //     }
+        // }
+        // return cnt;
         vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g = 0; g < n; g++) {
-            for(int i = 0, j = g; j < n; i++, j++) {
-                solve(dp, i, j, s);
-                if(dp[i][j] == true) {
-                    cnt++;
+        for(int l = 1; l <= n; l++)
+        {
+            for(int i = 0; i + l - 1 < n; i++)
+            {
+                int j = i + l - 1;
+                if(i == j)
+                {
+                    dp[i][j] = true;
+                }
+                else if(i+1 == j)
+                {
+                    if(s[i] == s[j])    dp[i][j] = true;
+                }
+                else
+                {
+                    if(s[i] == s[j] && dp[i+1][j-1] == true)
+                    {
+                        dp[i][j] = true;
+                    }
                 }
             }
         }
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = i; j < n; j++)
+                if(dp[i][j])
+                    cnt++;
+
         return cnt;
     }
 };
