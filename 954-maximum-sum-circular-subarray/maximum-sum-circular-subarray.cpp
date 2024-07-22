4036c043bda04f81692b2e4346f797d6f96d7355
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-22 22:59:58 +0530

    Time: 43 ms (69.21%) | Memory: 42.5 MB (27.43%) - LeetSync

---
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        
+        int n = nums.size();
+        int totalSum = nums[0], ans = nums[0];
+
+        int sum = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            sum += nums[i];
+            totalSum = max(totalSum + nums[i], nums[i]);
+            ans = max(ans, totalSum);
+        }
+
+        cout << "ans - " << ans << endl;
+
+        totalSum = nums[0];
+        int mini = nums[0];
+
+        for(int i = 1; i < n; i++)
+        {
+            totalSum = min(totalSum + nums[i], nums[i]);
+            mini = min(mini, totalSum);
+        }
+
+        if(ans > 0)
+            return max(ans, sum - mini);
+        return ans;
+    }
+};
