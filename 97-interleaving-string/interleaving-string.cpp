2257690107fe5047c2500438b65458b7de15e57e
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-22 23:30:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (46.86%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2, int ind, string &s1, string &s2, string &s3, vector<vector<vector<int>>>& dp)
+    int solve(int ind1, int ind2, int ind, string &s1, string &s2, string &s3, vector<vector<int>>& dp)
     {
         //  base cases
         if(ind1 >= s1.size() && ind2 >= s2.size()) {
@@ -10,21 +10,21 @@ public:
 
         if(ind1 > s1.size() || ind2 > s2.size())    return 0;
 
-        if(dp[ind1][ind2][ind] != -1)
-            return dp[ind1][ind2][ind];
+        if(dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
         
         if(s1[ind1] == s3[ind] && s2[ind2] == s3[ind]) {
-            return dp[ind1][ind2][ind] = (solve(ind1 + 1, ind2, ind + 1, s1, s2, s3, dp) 
+            return dp[ind1][ind2] = (solve(ind1 + 1, ind2, ind + 1, s1, s2, s3, dp) 
              | solve(ind1, ind2 + 1, ind + 1, s1, s2, s3, dp));
         }
 
         if(s1[ind1] == s3[ind])
-            return dp[ind1][ind2][ind] = solve(ind1 + 1, ind2, ind + 1, s1, s2, s3, dp);
+            return dp[ind1][ind2] = solve(ind1 + 1, ind2, ind + 1, s1, s2, s3, dp);
         
         if(s2[ind2] == s3[ind])
-            return dp[ind1][ind2][ind] = solve(ind1, ind2 + 1, ind + 1, s1, s2, s3, dp);
+            return dp[ind1][ind2] = solve(ind1, ind2 + 1, ind + 1, s1, s2, s3, dp);
         
-        return dp[ind1][ind2][ind] = 0;
+        return dp[ind1][ind2] = 0;
     }
 
     bool isInterleave(string s1, string s2, string s3) {
@@ -35,7 +35,7 @@ public:
 
         if(n+m != t)    return false;
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(t+1, -1)));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
 
         return solve(0, 0, 0, s1, s2, s3, dp);
     }
