47a264c4c9b1955d34faf1d0d076beca6993fc7a
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-07-30 11:41:06 +0530

    Time: 104 ms (40.03%) | Memory: 24.6 MB (54.55%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         
+        //  can be done using prefix sum for left b and right a for index i
         stack<char> st;
         int cnt = 0;
         for(char c: s) {
