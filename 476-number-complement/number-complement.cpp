18e3f29adc3488cb12fe6b1d7e9a57951d82104e
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2024-08-22 13:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (8.34%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        
+        int ans = 0;
+        int i = 0;
+        while(num > 0)
+        {
+            if(num & 1) {
+                ans = ans | (0 << i);
+            }
+            else {
+                ans = ans | (1 << i);
+            }
+            num = num >> 1;
+            i++;
+        }
+        return ans;
+    }
+};
